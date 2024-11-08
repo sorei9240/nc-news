74896fa3a41f6ab09ed198e63656d5adab4386d7
Haley Visconti <hvisconti1343@outlook.com>
2024-11-08 18:26:30 +0000

    fixed margin on article page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -46,7 +46,7 @@ const Article = () => {
     }
 
     return(
-        <div className="max-w-3xl mx-auto p-4">
+        <div className="max-w-3xl mx-auto p-4 mt-5">
             <div className="bg-slate-800 p-5 mb-4 rounded-lg">
                 <h2 className='text-xl font-semibold text-white'>{article.title}</h2>
                 <p className='text-sm mb-4 text-gray-300'>
